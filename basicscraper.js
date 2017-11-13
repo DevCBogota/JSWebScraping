ec151dc830533875d69b9e271a215cdf34bf700a
estebandalelr <estebandalelr@gmail.com>
2017-11-13 16:37:49 -0500

    Corrected a function

--- basicscraper.js
+++ basicscraper.js
@@ -5,10 +5,10 @@ let scrape = async () => {
   const page = await browser.newPage();
   await page.goto('http://books.toscrape.com/');
   await page.waitFor(1000);
+  await page.click(
+    "#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a > img"
+  )
   const result = await page.evaluate(() => {
-    await page.click(
-      "#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a > img"
-    )
     let title = document.querySelector('h1').innerText;
     let price = document.querySelector('.price_color').innerText;
     return {
