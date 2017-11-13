4ef34b2b26cf4a4d1b57adbd63cb93a82baa6025
estebandalelr <estebandalelr@gmail.com>
2017-11-13 16:11:40 -0500

    Created the file to take a screenshot

---
+++ screenshot.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+
+async function getPic() {
+  const browser = await puppeteer.launch({headless: false});
+  // const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+  // await page.setViewport({width: 1000, height: 500})
+  await page.screenshot({path: 'google.png'});
+
+  await browser.close();
+}
+
+getPic();
