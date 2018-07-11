2880c29b0ec7f3cdfc7de320d08af4defbbae182
Diego Coy <diegocoyt@gmail.com>
2018-07-11 18:34:51 -0500

    chore: create folders for every sample. Rename the samples
    chore: remove commented code. Create node tasks for every sample
    
    docs: update readme. closes #7, closes #5, bases for #8, helps with #2

--- basico/index.js
+++ basico/index.js
@@ -1,10 +1,12 @@
 const puppeteer = require('puppeteer');
 
-let scrape = async () => {
-  // const browser = await puppeteer.launch({headless: false});
+const targetURL = 'http://books.toscrape.com/';
+
+const scrape = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://books.toscrape.com/');
+
+  await page.goto(targetURL);
   await page.waitFor(1000);
   await page.click(
     "#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a > img"
@@ -12,6 +14,7 @@ let scrape = async () => {
   const result = await page.evaluate(() => {
     const title = document.querySelector('h1').innerText;
     const price = document.querySelector('.price_color').innerText;
+    
     return {
       title,
       price
@@ -23,5 +26,5 @@ let scrape = async () => {
 };
 
 scrape().then((value) => {
-  console.log(value); // Success!
+  console.log(value);
 });
