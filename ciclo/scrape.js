b6de904e73abc92d15e4c6cefb5db05843f60bec
simpleap <gerlis1997@gmail.com>
2018-10-11 18:50:22 -0500

    Added args for circleci

--- ciclo/scrape.js
+++ ciclo/scrape.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 const scrape = async (targetURL, elementsSelector, evaluateFunction) => {
   // const browser = await puppeteer.launch({headless: false});
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['–no-sandbox', '–disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.goto(targetURL);
   const result = await page.evaluate(evaluateFunction, elementsSelector);
